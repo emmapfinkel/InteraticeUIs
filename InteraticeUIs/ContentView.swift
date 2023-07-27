3825b5f25012a57fc8258797fa7c52942dcf1eb3
Emma Finkel <emma.finkel@gmail.com>
2023-07-27 13:00:55 -0400

    Initial Commit

---
+++ InteraticeUIs/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  InteraticeUIs
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack {
+            
+            Text(textTitle)
+            
+            TextField("Type name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+        }
+        .padding()
+       
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteraticeUIs/InteraticeUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteraticeUIsApp.swift
+//  InteraticeUIs
+//
+//  Created by scholar on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct InteraticeUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
